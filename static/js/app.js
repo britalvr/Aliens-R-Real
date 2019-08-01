8ae1ebb191d92540e53666c35e85ebd910a19648
shewolf <britalvr@gmail.com>
2019-07-31 19:40:12 -0500

    --

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,16 @@
 // from data.js
+
 var table_Data = data;
 
 // YOUR CODE HERE!
+
+
 // console.log(table_Data);
 
 
 let tbody = d3.select("tbody");
 let table = d3.select("table");
+
 
 
 table.attr("class", "table table-striped");
@@ -26,8 +30,11 @@ function build_Table(table_Data) {
       let UFO_data = row.append("td");
 
         UFO_data.text(value);
+
       });
+
   });
+
 };
 
 
@@ -47,25 +54,34 @@ let inputState = d3.select("#state");
 let inputCountry = d3.select("#country");
 let inputShape = d3.select("#shape");
 
+
 // function buttonClick() {
+
 submit.on("click", function() {
 
   d3.event.preventDefault();
 
+
 // let date = d3.select("#datetime").property("value");
+
   let input_Value = inputDate.property("value");
   console.log(input_Value);
 
 //      let data_Filtered = table_Data;
 
+
 //      if (date) {
+
 //        data_Filtered = data_Filtered.filter(row => row.datetime === date);
 //      }
 
+
 //      table(data_Filtered);
+
   let data_Filtered = table_Data.filter(UFO_report => UFO_report.datetime == input_Value);
   console.log(data_Filtered);
  
+
 
   build_Table(data_Filtered);
 
